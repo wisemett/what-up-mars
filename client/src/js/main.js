4fa0e0c41be8bec6631620c3f54958d6a8dfa2e0
hyorard-b <reddream08@gmail.com>
2021-02-05 12:56:32 +0900

    bugfix: Fix False Import

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,7 +1,9 @@
 import renderDday from './render/d-day';
 import renderWeather from './render/weather';
 import renderPictures from './render/pictures';
+import renderTweets from './render/tweets';
 
 renderDday(2030, 1, 1);
 renderWeather();
 renderPictures();
+renderTweets();
