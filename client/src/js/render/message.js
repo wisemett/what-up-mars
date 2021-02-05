97d46918df58e51d6820342cff6f02c965f0df23
hyorard-b <reddream08@gmail.com>
2021-02-05 16:08:08 +0900

    feat: Render Post Time & Random Profile Img

--- client/src/js/render/message.js
+++ client/src/js/render/message.js
@@ -1,4 +1,5 @@
 import message from '../axios/message';
+import formatTime from '../utils/formatMessage';
 
 const $msgList = document.querySelector('.cheer__list');
 const $submitBtn = document.querySelector('.submit-msg');
@@ -11,44 +12,35 @@ const watchPost = async e => {
   renderMessage();
 };
 
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+};
+
 const renderMessage = async () => {
   const res = await message.get();
   messages = [...res];
 
-  messages = messages.map(({name, contents, time}) => {
-    const fTime = formatTime(time);
-    return {name, contents, fTime};
-  });
+  messages = formatTime(messages);
   
   $msgList.innerHTML = '';
   messages.forEach(msg => {
+    const randomNum = getRandomInt(0,6)
     $msgList.innerHTML += `
     <li class="cheer__item">
-      <div class="profile-img"></div>
+      <div class="profile-img" style="background:url('../../img/profile/${randomNum}.jpg')"></div>
         <div class="user-info">
           <span class="user-name">${msg.name}</span>
           <span class="user-contents">${msg.contents}</span>
         </div>
-      <span class="cheer-time">${msg.fTime.year}-${msg.fTime.month}-${msg.fTime.day}</span>
+      <span class="cheer-time">${msg.fTime[0]} ${msg.fTime[1]} ago</span>
     </li> `
+
   });
+
+
+  
   
   $submitBtn.onclick = watchPost;
-  
 };
-
-const formatTime = time => {
-  const then = new Date(time);
-
-  const year = then.getFullYear();
-  const month = then.getMonth();
-  const day = then.getDay();
-
-  return {
-    year,
-    month,
-    day
-  };
-}
 
 export default renderMessage;
--- client/src/js/utils/formatMessage.js
+++ client/src/js/utils/formatMessage.js
@@ -1,4 +1,20 @@
-messages.map(({name, contents, time}) => {
-  const fTime = formatTime(time);
-  return {name, contents, fTime};
-});
+import moment from 'moment';
+
+const getGap = time => {
+  const then = moment(time);
+  const now = moment();
+
+  if (now.diff(then, 'years') > 0) return [now.diff(then, 'years'), 'year'];
+  else if (now.diff(then, 'months') > 0) return [now.diff(then, 'months'), 'month'];
+  else if (now.diff(then, 'days') > 0) return [now.diff(then, 'days'), 'day'];
+  else if (now.diff(then, 'hours') > 0) return [now.diff(then, 'hours'), 'hour'];
+  else if (now.diff(then, 'minutes') > 0) return [now.diff(then, 'minutes'), 'minute'];
+  else return [now.diff(then, 'seconds'), 'seconds'];
+};
+
+const formatTime = messages => messages.map(({name, contents, time}) => {
+    const fTime = getGap(time);
+    return {name, contents, fTime};
+  });
+
+export default formatTime;
