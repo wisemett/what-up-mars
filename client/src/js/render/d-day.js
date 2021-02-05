a2cbfb46a98e313aa9fc871fb62967f9116e4aae
hyorard-b <reddream08@gmail.com>
2021-02-05 16:38:38 +0900

    feat: Render Sol Date On Header

--- client/src/js/render/d-day.js
+++ client/src/js/render/d-day.js
@@ -1,11 +1,14 @@
 import setdDay from '../utils/set-d-day';
 import countDday from '../utils/count-d-day';
+import makeInsightObject from '../axios/weather';
 
 const $dDay = document.querySelector('.d-day');
 const $dDate = document.querySelector('.d-date');
+const $solDate = document.querySelector('.sol');
 
-const renderDday = (year, month, day) => {
+const renderDday = async (year, month, day) => {
   const dDay = setdDay(year, month, day);
+  const {solDate} = await makeInsightObject();
 
   setInterval(() => {
     // 카운트
@@ -14,6 +17,8 @@ const renderDday = (year, month, day) => {
     $dDay.textContent = `${countedDday.dYear}y ${countedDday.dMonth}m ${countedDday.dDay}d`;
     $dDate.textContent = `${countedDday.dHour}:${countedDday.dMinutes}:${countedDday.dSeconds}`;
   }, 1000);
+
+  $solDate.textContent = ''+ solDate;
 }
 
 export default renderDday;
